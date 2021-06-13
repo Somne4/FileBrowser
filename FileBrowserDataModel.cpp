fea21a6f4bb3e403638fb2cebc125d6c791e336c
Farxad <favourite_king@mail.ru>
2021-06-13 20:14:14 +0700

    Добавление интерфейса класса

---
+++ FileBrowserDataModel.cpp
@@ -0,0 +1,82 @@
+#include "FileBrowserDataModel.h"
+
+
+FileBrowserDataModel::FileBrowserDataModel(QObject *parent, QList<SomeData> dt) : QAbstractTableModel(parent)
+{
+    dataModel = dt;
+}
+
+void FileBrowserDataModel::setModel(const QList<SomeData> &dt)
+{
+    beginResetModel();
+    dataModel = dt;
+    endResetModel();
+}
+
+//определение количества строк в модели;
+
+int FileBrowserDataModel::rowCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent);
+    return dataModel.count();
+}
+
+//определение количества столбцов в модели
+
+int FileBrowserDataModel::columnCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+    return PERCENT + 1;
+}
+
+//Возвращаем названия заголовков.
+
+QVariant FileBrowserDataModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (role != Qt::DisplayRole)
+    {
+        return QVariant();
+    }
+
+    if (orientation == Qt::Vertical)
+    {
+        return section;
+    }
+
+    switch (section)
+    {
+        case NAME:
+            return QString::fromUtf8("Name");
+        case NameColumn::SIZE:
+            return QString::fromUtf8("Size");
+        case PERCENT:
+            return QString::fromUtf8("Percentage");
+    }
+    return QVariant();
+}
+
+QVariant FileBrowserDataModel::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid() || dataModel.count() <= index.row() || (role != Qt::DisplayRole && role != Qt::EditRole))
+    {
+        return QVariant();
+    }
+
+    if (index.column() == 0)
+    {
+        return dataModel[index.row()].name;
+    }
+    else if (index.column() == 1)
+    {
+        return dataModel[index.row()].size;
+    }
+    else if (index.column() == 2)
+    {
+        return dataModel[index.row()].percent;
+    }
+    else
+        return QVariant();
+}
+
+
+
---
+++ FileBrowserDataModel.h
@@ -0,0 +1,30 @@
+#ifndef FILEBROWSERDATAMODEL_H
+#define FILEBROWSERDATAMODEL_H
+#include <QAbstractTableModel>
+#include <QList>
+#include <QLocale>
+#include "Browser.h"
+
+class FileBrowserDataModel : public QAbstractTableModel
+{
+    Q_OBJECT
+private:
+    enum NameColumn //Определим перечисление NameColumn для индексации столбцов нашей таблицы.
+    {
+        NAME = 0,
+        SIZE,
+        PERCENT
+    };
+    QList<SomeData> dataModel; //Представим данные модели в виде списка.
+public:
+    FileBrowserDataModel() = default;
+    FileBrowserDataModel(QObject *parent = nullptr, QList<SomeData> dt = QList<SomeData>());
+    void setModel(const QList<SomeData>& dt);
+    ~FileBrowserDataModel() = default;
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex& parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+};
+
+#endif // FILEBROWSERDATAMODEL_H
